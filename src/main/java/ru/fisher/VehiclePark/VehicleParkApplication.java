4b7c7215519e5a66bc63a45aa807f75fd5bfe426
Fisher48 <rybakov48@yandex.ru>
2024-11-04 20:02:31 +0300

    added ModelMapper

--- src/main/java/ru/fisher/VehiclePark/VehicleParkApplication.java
+++ src/main/java/ru/fisher/VehiclePark/VehicleParkApplication.java
@@ -1,13 +1,22 @@
 package ru.fisher.VehiclePark;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class VehicleParkApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VehicleParkApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 }
