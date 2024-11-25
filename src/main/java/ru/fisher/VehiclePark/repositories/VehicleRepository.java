247b6ef3e97830b8e78a087dea9a6df529d24b06
Fisher48 <rybakov48@yandex.ru>
2024-11-25 22:36:45 +0300

    changed in VehicleRepository

--- src/main/java/ru/fisher/VehiclePark/repositories/VehicleRepository.java
+++ src/main/java/ru/fisher/VehiclePark/repositories/VehicleRepository.java
@@ -1,14 +1,24 @@
 package ru.fisher.VehiclePark.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import ru.fisher.VehiclePark.models.Enterprise;
 import ru.fisher.VehiclePark.models.Vehicle;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface VehicleRepository extends JpaRepository<Vehicle, Integer> {
+public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
+
+   List<Vehicle> findVehiclesByEnterpriseId(Long id);
 
    Optional<Vehicle> findByNumber(String vehicleName);
+
+   List<Vehicle> findAllByEnterpriseIn(List<Enterprise> enterprises);
+
+   Page<Vehicle> findAllByEnterprise(Enterprise enterprise, Pageable pageable);
 
 }
