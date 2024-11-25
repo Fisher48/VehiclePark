2890ce9028fd5f19a0a9927c7eed57ce2e093652
Fisher48 <rybakov48@yandex.ru>
2024-11-25 22:35:46 +0300

    added DriverVehicle

---
+++ src/main/java/ru/fisher/VehiclePark/models/DriverVehicle.java
@@ -0,0 +1,21 @@
+package ru.fisher.VehiclePark.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(schema = "autopark", name = "driver_vehicle") // Укажите схему, если необходимо
+public class DriverVehicle {
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "driver_id")
+    private Driver driver;
+
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "vehicle_id")
+    private Vehicle vehicle;
+
+
+}
