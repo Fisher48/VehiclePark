8a413f5b2509ca916f538081c6623473c75aaa51
Fisher48 <rybakov48@yandex.ru>
2024-11-25 22:35:27 +0300

    added JacksonConfig

---
+++ src/main/java/ru/fisher/VehiclePark/config/JacksonConfig.java
@@ -0,0 +1,31 @@
+package ru.fisher.VehiclePark.config;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import org.openapitools.jackson.nullable.JsonNullableModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class JacksonConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converters.add(converter);
+    }
+
+    @Bean
+    Jackson2ObjectMapperBuilder objectMapperBuilder() {
+        var builder = new Jackson2ObjectMapperBuilder();
+        builder.serializationInclusion(JsonInclude.Include.NON_NULL)
+                .modulesToInstall(new JsonNullableModule());
+        return builder;
+    }
+
+}
